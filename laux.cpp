4cdb61da3ea37301a0116fa663b1fff9b55b53b6
speedsterkawaii <lovevirdi037@gmail.com>
2025-01-18 23:18:15 -0500

    Update laux.cpp

--- laux.cpp
+++ laux.cpp
@@ -1,5 +1,4 @@
 
-// note i used luastate as first parameter bc i when I personally grab lua state i dont store to a int or dword or anything i cast to luastate FYI so you may need to change that part if you want its simple lol
 
 using luaL_register_t = void(__fastcall*)(lua_State* L, const char* libname, const luaL_Reg* l); // it will show as int for first arg, char for second and int again for third, thats just the pseudo. originally, this was meant for use with shuffles. 
 luaL_register_t luaL_register = reinterpret_cast<luaL_register_t>(0x0); // _LOADED search this string and you will see it you cannot miss it
@@ -28,4 +27,4 @@ luaL_register_t luaL_register = reinterpret_cast<luaL_register_t>(0x0); // _LOAD
 // your final code should be smth like this
 void main(){
 shit(luastatethingie);
-end}
+}; //oops do not check commit history i thought it was luau (dementia problems 100%)
